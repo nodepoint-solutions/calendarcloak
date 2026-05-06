e7acaa9112c8543c9a1db4e90f2b168e17fc0c65
Alex Luckett <alex@nodepoint.co.uk>
2026-05-06 23:46:19 +0100

    feat: add UpdateState enum to AppState

--- CalendarCloak/State/AppState.swift
+++ CalendarCloak/State/AppState.swift
@@ -1,10 +1,19 @@
 import Foundation
 import Observation
 
+enum UpdateState: Equatable {
+    case idle
+    case available(version: String, dmgUrl: URL)
+    case downloading(pct: Double)
+    case installing
+    case restarting
+}
+
 @Observable
 final class AppState {
     var lastSyncDate: Date?
     var activeCalendarNames: [String] = []
     var isAccessDenied: Bool = false
     var errorMessage: String?
+    var updateState: UpdateState = .idle
 }
--- CalendarCloakTests/UpdateStateTests.swift
+++ CalendarCloakTests/UpdateStateTests.swift
@@ -1,1 +1,37 @@
 import XCTest
+@testable import CalendarCloak
+
+final class UpdateStateTests: XCTestCase {
+    func test_idle_equatable() {
+        XCTAssertEqual(UpdateState.idle, UpdateState.idle)
+    }
+
+    func test_available_equatable_same() {
+        let url = URL(string: "https://example.com/update.dmg")!
+        XCTAssertEqual(
+            UpdateState.available(version: "1.2.3", dmgUrl: url),
+            UpdateState.available(version: "1.2.3", dmgUrl: url)
+        )
+    }
+
+    func test_available_equatable_different_version() {
+        let url = URL(string: "https://example.com/update.dmg")!
+        XCTAssertNotEqual(
+            UpdateState.available(version: "1.2.3", dmgUrl: url),
+            UpdateState.available(version: "1.2.4", dmgUrl: url)
+        )
+    }
+
+    func test_downloading_equatable() {
+        XCTAssertEqual(UpdateState.downloading(pct: 0.5), UpdateState.downloading(pct: 0.5))
+        XCTAssertNotEqual(UpdateState.downloading(pct: 0.5), UpdateState.downloading(pct: 0.9))
+    }
+
+    func test_idle_not_equal_installing() {
+        XCTAssertNotEqual(UpdateState.idle, UpdateState.installing)
+    }
+
+    func test_restarting_equatable() {
+        XCTAssertEqual(UpdateState.restarting, UpdateState.restarting)
+    }
+}
